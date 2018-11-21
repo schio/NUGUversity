50e3b080724a3d1098929b9630c8b92d7f98b859
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-11-22 08:39:08 +0900

    Cafeteria response

--- app.py
+++ app.py
@@ -3,30 +3,57 @@ from flask import request
 from pprint import pprint as p
 
 app = Flask(__name__)
- 
+def get_leaf_dict( dict, key_list):
+    res=dict
+    for key in key_list:
+        res=dict.setdefault( key, {} )
+    return res
+
 @app.route('/nugu/<svc>', methods=['POST'])
 def start(svc):
     data = request.json
 
     # actionData
     action = data['action']
+    params = action['parameters']
+    actionName = action['actionName']
 
-    if action['actionName'] == 'answer.meal_price':
-        params = action['parameters']
-        p(params)
+    # 학식 가격
+    if actionName == 'answer.meal_price':
         mm = params['meal_name']
-        p(mm)
-        data = mm['value']
-        p(data)
-        result = meal_price(data)
+        name = mm['value']
+        result = meal_price(name)
 
-    elif action['actionName'] == 'answer.which_cafeteria':
-        result = 0
+    # 학식 메뉴(식당별)
+    elif actionName == 'answer.which_cafeteria':
+        which = params['which_2']
+        name = which['value']
+        result = which_cafeteria(name)
 
-    return result
+    # 학식 메뉴(식당별, 날짜별))
+    elif actionName == 'answer.which_when_cafeteria':
+        which = params['which']
+        when = params['when']
+        name = which['value']
+        day = when['value']
+        result = which_when cafeteria(name, day)
+
+    result_dict = {
+        "version": "2.0",
+        "resultCode": "OK",
+        "output": result,
+    }
+    return result_dict
+
 
 def meal_price(name):
-    return 4500
+    return {'price': '4500'}
+
+def which_cafeteria(name):
+    return {'menu_2': '동까스'}
+
+def which_when_cafeteria(name, day):
+    return {'menu': '동까스2'}
 
 @app.route('/')
 def hello_world():
