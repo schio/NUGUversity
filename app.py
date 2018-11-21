5e5ca5b7f107d302c938cd17208684be26fd040d
schio <ghsehr1@gmail.com>
2018-11-22 05:36:43 +0900

    solve conflict

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route('/start')
+def start():
+    return 'start'
+ 
+ 
+@app.route('/select/<name>')
+def select(name):
+    return 'hi %s' % name
+ 
+ 
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World!</h1><input type="textbox"/>'
+ 
+ 
+if __name__ == '__main__':
+    app.run()
