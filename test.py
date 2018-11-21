2f0791a09f0469aef199a26d1a82a589fcbd9db4
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-11-22 01:06:27 +0900

    test.py Parsing

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful import reqparse
  
 app = Flask(__name__)
 api = Api(app)
  
 class CreateUser(Resource):
     def post(self):
-        return {'status': 'success'}
+        try:
+            parser = reqparser.RequestParser()
+            parser.add_argument('email', type=str)
+            parser.add_argument('user_name', type=str)
+            parser.add_argument('password', type=str)
+            args = parser.parse_args()
+
+            return {
+                'Email': args['email'],
+                'UserName': args['user_name'],
+                'Password': args['password']
+            }
+        except Exception as e:
+            return {'error': str(e)}
  
 api.add_resource(CreateUser, '/scio')
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
