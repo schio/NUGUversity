5b9b850e9b3f16ae7925e280b720cdbb964519f6
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-11-22 05:39:35 +0900

    solve conflict - app.py

--- app.py
+++ app.py
@@ -2,22 +2,9 @@ from flask import Flask
  
 app = Flask(__name__)
  
- 
-<<<<<<< HEAD
-@app.route('/start')
-def start():
-    return 'start'
- 
- 
-@app.route('/select/<name>')
-def select(name):
-    return 'hi %s' % name
- 
-=======
 @app.route('/nugu/<svc>')
 def start(svc):
     return 'type : %s' % svc
->>>>>>> c645cbbdb42d90b14e90e938673761bf26fc545b
  
 @app.route('/')
 def hello_world():
@@ -25,8 +12,4 @@ def hello_world():
  
  
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run()
-=======
-    app.run()
->>>>>>> c645cbbdb42d90b14e90e938673761bf26fc545b
